7d9bb574cfc8a4479006036ba51f9b129dc55fb4
Ghezal ilies <43371570+Ghz4070@users.noreply.github.com>
2021-06-29 10:51:09 +0200

    Update wiki.pp

--- wiki.pp
+++ wiki.pp
@@ -16,7 +16,7 @@ exec {
     'extract dokuwiki':
         command => 'tar xavf dokuwiki.tgz',
         cwd => '/usr/src',
-        path => ['/usr/bin', '/usr/sbin'],
+        path => ['/usr/bin/tar'],
         require => File['download dokuwiki']
 }
 file {
